df92a944641e4c13a2748c6da3d7db3e9dfc08e0
Fernando Corrêa Gambôa Pereira dos Santos <fcgps@cin.ufpe.br>
2026-09-19 23:49:39 -0300

    Cria a rota (Get /turmas/{id_turma)/alunos)(#10)

--- backend/src/turmas/turmas.controller.ts
+++ backend/src/turmas/turmas.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { TurmasService } from './turmas.service.js';
 
-@Controller('turmas')
-export class TurmasController {}
+@Controller('turmas') // Primeira parte do endereço (/turmas)
+export class TurmasController {
+    constructor(private readonly turmasService: TurmasService){}
+
+    @Get(':id_turma/alunos')  // Segunda parte (/:id_turma/alunos)
+    listarAlunos(@Param('id_turma') idTurma : string ) { // Método para listar alunos
+        return {
+            mensagem : "Rota alcançada com êxito", turma_solicitada: idTurma
+        };
+    }
+}
